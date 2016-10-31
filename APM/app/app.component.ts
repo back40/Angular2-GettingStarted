7de324dc44ba447c8d87d9a6f27b0ac5cfdc27e0
back40 <conk27@yahoo.com>
2016-10-31 11:21:12 -0700

    init

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './products/product.service'
+
 @Component({
     selector: 'pm-app',
+    moduleId: module.id,
     template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+    <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Product List</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class='container-fluid'>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+     `, 
+    providers: [ ProductService ]
 })
-export class AppComponent { }
+export class AppComponent { 
+    pageTitle: string = "Acme Product Management";
+}
